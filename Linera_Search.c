dbd7cf3ad895c9c5bae60b2a0a79b6551229ad9c
Ronak Singh <111114934+ronaksingh93575@users.noreply.github.com>
2022-11-26 12:06:09 +0530

    Add files via upload

---
+++ Linera_Search.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+    int size;
+    int arr[100];
+    int search;
+    printf("Enter size of array :- ");
+    scanf("%d", &size);
+    for (int i = 0; i < size; i++)
+    {
+        printf("Insert Element :- ");
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter value want to be seacrh :- ");
+    scanf("%d", &search);
+
+    for (int i = 0; i < size; i++)
+    {
+        if (search == arr[i])
+        {
+            printf("Value found at laoction %d\n", i);
+            break;
+        }
+        else
+            printf("!!!! Value not found\n");
+    }
+    // insertion(arr[100],size,search);
+    return 0;
+}
+// int insertion(int arr[100],int size, int search)
+// {
+// }
